d8692452dca9dbcdc03a6e66353eec7221f953c6
Cubee-kun <ibraveaslam73@gmail.com>
2025-11-07 15:48:56 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -213,7 +213,7 @@ export default function App() {
                       <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
                       <circle cx="12" cy="10" r="3" />
                     </svg>
-                    <span>Jl. Bukit Jarian dlm VI, Bandung</span>
+                    <span>Jl. Bukit Jarian dlm VI No. 30, Kota Bandung</span>
                   </li>
                   <li className="flex items-start gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="mt-1">
