fb94a79dbbc86cbd10191b56d25629f10b556221
Cubee-kun <ibraveaslam73@gmail.com>
2026-03-06 07:35:35 +0700

    update load time biar ga lemot

--- vite.config.ts
+++ vite.config.ts
@@ -21,10 +21,37 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false,
     minify: 'terser',
+    chunkSizeWarningLimit: 900,
     rollupOptions: {
       output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        manualChunks(id) {
+          if (!id.includes('node_modules')) return
+
+          if (/node_modules\/(react|react-dom|react-router-dom|react-helmet-async)\//.test(id)) {
+            return 'react-vendor'
+          }
+
+          if (/node_modules\/three\//.test(id)) {
+            return 'three-core'
+          }
+
+          if (/node_modules\/@react-three\/(fiber|drei)\//.test(id)) {
+            return 'three-react'
+          }
+
+          if (/node_modules\/(gsap|animejs)\//.test(id)) {
+            return 'animation-vendor'
+          }
+
+          if (/node_modules\/(react-icons|lucide-react)\//.test(id)) {
+            return 'icons-vendor'
+          }
+
+          if (/node_modules\/axios\//.test(id)) {
+            return 'network-vendor'
+          }
+
+          return
         }
       }
     }
